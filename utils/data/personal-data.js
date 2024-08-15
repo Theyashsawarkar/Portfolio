f1a3d3af574ad33bd6a04ede942f542239d2f795
yash sawarkar <yashsawarkar01@gmail.com>
2024-08-15 15:29:25 +0530

    initial commit

---
+++ utils/data/personal-data.js
@@ -0,0 +1,15 @@
+export const personalData = {
+  name: "Yash Sawarkar",
+  profile: '/profile.png',
+  designation: "Software Developer",
+  email: 'yashsawarkar01@gmail.com',
+  phone: '+917507405875',
+  address: 'Main Road Dahigaon Recha AnjanGoan surji , Dist. Amaravati , Maharashtra , India',
+  github: 'https://github.com/Theyashsawarkar',
+  instagram: 'https://www.instagram.com/Theyashsawarkar',
+  linkedIn: 'https://www.linkedin.com/in/theyashsawarkar/',
+  twitter: 'https://twitter.com/theyashsawarkar',
+  coding_ninjas: "https://www.codingninjas.com/studio/profile/Theyashsawarkar",
+  // TODO: update resume link 
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
