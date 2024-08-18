f322f98a70ea36dcda409a517fcbfe20a67b235a
yash sawarkar <yashsawarkar01@gmail.com>
2024-08-18 18:02:12 +0530

    basic app is working fine

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,12 +4,13 @@ export const personalData = {
   designation: "Software Developer",
   email: 'yashsawarkar01@gmail.com',
   phone: '+917507405875',
-  address: 'Main Road Dahigaon Recha AnjanGoan surji , Dist. Amaravati , Maharashtra , India',
+  address: 'Main Road Dahigaon Recha AnjanGoan Surji, Dist. Amaravati, Maharashtra, India . 444705',
   github: 'https://github.com/Theyashsawarkar',
   instagram: 'https://www.instagram.com/Theyashsawarkar',
   linkedIn: 'https://www.linkedin.com/in/theyashsawarkar/',
   twitter: 'https://twitter.com/theyashsawarkar',
   coding_ninjas: "https://www.codingninjas.com/studio/profile/Theyashsawarkar",
+  devUsername: 'theyashsawarkar',
   // TODO: update resume link 
   resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
 }
