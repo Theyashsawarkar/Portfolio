2f29b11c44e0395643185cbb8fbd89708e94eebb
yash sawarkar <yashsawarkar01@gmail.com>
2024-09-29 02:18:08 +0530

    trying to fix the cover images not loading issue

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
-const path = require('path')
- 
+const path = require('path') 
+
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
@@ -8,12 +8,17 @@ module.exports = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        hostname: 'res.cloudinary.com', // For Cloudinary images
         pathname: '**',
       },
       {
         protocol: 'https',
-        hostname: 'media.dev.to',
+        hostname: 'media.dev.to', // For dev.to images served via CDN
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dev-to-uploads.s3.amazonaws.com', // For dev.to's S3-hosted images
         pathname: '**',
       },
     ],
